4da9ed8734969576a75f050feee22cdbf4e76cee
Santosh Ghimire <sg606618@gmail.com>
2024-02-22 12:03:34 +0545

    under construction creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
 
-
 export default function Home() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <div className="container font-bold text-3xl text-red-500 bg-transparent w-[100vw] h-[100vh] flex justify-center items-center">
+        Under Construction
+      </div>
     </>
   );
 }
