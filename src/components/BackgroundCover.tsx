d410315131b33dcf88709fd5bd6ec4bfedb77529
Santosh Ghimire <sg606618@gmail.com>
2024-02-23 16:51:29 +0545

    more content

--- src/components/BackgroundCover.tsx
+++ src/components/BackgroundCover.tsx
@@ -1,26 +1,18 @@
 "use client";
 
-import { Roboto } from "next/font/google";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import React from "react";
-
-const roboto = Roboto({
-  weight: "400", // if single weight, otherwise you use array like [400, 500, 700],
-  style: "normal", // if single style, otherwise you use array like ['normal', 'italic']
-  subsets: ["latin"],
-});
 
 const BackgroundCover = () => {
   return (
     <>
       <div className="w-full h-[500px] flex justify-between container mx-auto relative">
         <div className="absolute w-full h-full bg-black z-10 flex sm:hidden bg-opacity-80"></div>
-        {/* <div className="absolute w-full h-full bg-black -z-10 sm:flex hidden bg-opacity-10"></div> */}
         <div className="relative h-full w-full flex justify-end items-start flex-col pb-40 z-50 px-4">
           <section className="flex flex-col mb-5">
-            <h1 className="text-[2rem] sm:text-[3rem] md:text-[4rem] lg:text-[5rem] font-semibold font-signika -mb-5 md:-mb-8 tracking-wide text-white sm:text-black">
+            <h1 className="text-[2rem] sm:text-[3rem] md:text-[4rem] lg:text-[5rem] font-semibold font-signika -mb-4 md:-mb-8 tracking-wide text-white sm:text-black">
               JAKA NEPAL
             </h1>
             <p className="text-[1rem] sm:text-[1.3rem] md:text-[1.5rem] lg:text-[1.9rem] text-[#F6050B] font-caveat tracking-normal md:tracking-widest font-semibold">
